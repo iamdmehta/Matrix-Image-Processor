26da8a4039979614d0fb9b0fee1fbd2172aeb2e6
iamdmehta <devrocks957@gmail.com>
2026-07-24 09:55:02 +0530

    Implemented Brightness adjustment

---
+++ main.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+int pixels[64][64];
+FILE *fp;
+
+fp=fopen("lion_p2.pgm","r");
+if(fp==NULL) printf("Error in opening the file.\n");
+else printf("File opened successfully!\n");
+
+//to skip the first 3 lines of .pgm file because it just info about the contents of the file and not the contents of the file itself.
+char format[3];
+int width, height, maxval;
+
+fscanf(fp, "%2s", format);
+fscanf(fp, "%d %d", &width, &height);
+fscanf(fp, "%d", &maxval);
+
+// for loop to take the intensity of each pixel into a 64x64 matrix 
+for(int i=0;i<64;i++){
+    for(int j=0;j<64;j++){
+        fscanf(fp,"%d",&pixels[i][j]);
+    } 
+}
+
+int brightness;
+printf("Enter by how many notches you want to vary the brightness: \n");
+scanf("%d",&brightness);
+
+//for loop to vary the image brightness by the user inputted value
+for(int i=0;i<64;i++){
+    for(int j=0;j<64;j++){
+        pixels[i][j]+=brightness;
+        if(pixels[i][j]>255) pixels[i][j]=255;
+        else if(pixels[i][j]<0) pixels[i][j]=0;
+    } 
+}
+
+FILE *fp1=fopen("feature1.pgm","w");
+if(fp1==NULL) printf("Error in opening the file.\n");
+else printf("File created successfully!Ready to write to it.\n");
+fprintf(fp1,"P2\n64 64\n255\n");
+for(int i=0;i<64;i++){
+    for(int j=0;j<64;j++){
+        fprintf(fp1,"%d ",pixels[i][j]);
+    } 
+    fprintf(fp1,"\n");
+}
+printf("Writing to the file completed.\n");
+
+fclose(fp);
+fclose(fp1);
+
+system("magick feature1.pgm feature1_preview.png");
+printf("Preview successfully created.\n");
+}
